8c0cbd0625a49ddc7163f0b8be9b7a7b461e872d
JnSsanRico <juan0096150@gmail.com>
2024-06-07 23:34:47 -0500

    ya hice los cambio en pedidos y reportes, me esribe que le parece

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@ config();
 export const configDB = {
     host:process.env.MYSQLHOST,
     port:process.env.MYSQLPORT,
-    user:process.env.MYSQLUSER,
-    password:process.env.MYSQLPASSWORD,
-    database:process.env.MYSQLDATABASE
+    user:process.env.MYSQLUSER || 'root',
+    password:process.env.MYSQLPASSWORD || 'root',
+    database:process.env.MYSQLDATABASE ||'tartaro'
 }
