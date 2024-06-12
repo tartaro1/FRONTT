58cbbf76711d276a3d56cb61d8062a7274079aa5
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2024-06-11 21:16:28 -0500

    front y back separados

--- backend/app/server.js
+++ backend/app/server.js
@@ -7,25 +7,18 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import router from './routes/index.js';
 config();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const server = express();
 server.use(urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, "public")));
-
-
 server.use(express.json());
 server.set("view engine", "ejs");
-server.set("views", path.join(__dirname, 'views'));
+server.set("views", path.join(__dirname, '../../frontend/app/views'));
 server.use(cors());
-server.set('port', process.env.PORT || 3000);
-
+server.set('port', process.env.PORT || 9200);
 server.use("/dashboard", (req, res) => {
     res.render('views.dashboard.ejs');
 });
-
 server.use("/", router);
-
 export default server;
