7b6e2cdeb6c4e0cbff3d694705e98ef3216575fc
JnSsanRico <juan0096150@gmail.com>
2024-07-05 10:50:18 -0500

    funciona el productos

--- frontend/app/controllers/products.controller.js
+++ frontend/app/controllers/products.controller.js
@@ -2,28 +2,22 @@ export class ProductController {
     static getAll = async(req, res) => {
         try {
             const {category} = req.query;
+            let url = "https://ms-backend-tartaro.onrender.com/products";
             if (category) {
-                let datos = {}
-                await fetch(`https://ms-backend-tartaro.onrender.com/products?category=${category}`)
-                .then(response => response.json())
-                .then(data => {
-                    datos = data;
-                    res.render("views.resultsProduct.ejs", {products: datos})
-                })
-            } else {
-                let datos = {};
-                await fetch("https://ms-backend-tartaro.onrender.com/products")
-                .then(response => response.json())
-                .then(data => {
-                    datos = data;
-                    // res.render("views.products.ejs", {products: datos})
-                    res.render("pages/admin/products",{
-                        layout:"layouts/main-admin",
-                        title: 'Dashboard Products',
-                        product: datos
-                    });
-                })
+                url += `?category=${category}`;
             }
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            // Asegúrate de que products sea un array
+            const products = Array.isArray(data) ? data : data.products || [];
+
+            res.render("pages/admin/products", {
+                layout: "layouts/main-admin",
+                title: 'Dashboard Products',
+                products: products
+            });
         } catch (error) {
             console.error("Error fetching products:", error.message);
             res.status(500).send(error.message);
