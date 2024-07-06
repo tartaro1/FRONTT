1ad1b9bc1f0fdc8d1b3d87714b9c0156ce22330e
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2024-07-06 12:28:49 -0500

    Estructura buena

--- frontend/app/routes/routes.details.js
+++ frontend/app/routes/routes.details.js
@@ -3,5 +3,6 @@ import { DetailsController } from "../controllers/details.controller.js";
 const routesDetails = Router();
 
 routesDetails.get("/", DetailsController.getAll);
+
 export default routesDetails;
 
